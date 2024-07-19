9cddc4f1f4b95cabde413f98011fdb979a290c60
lazaralex98 <alex2012ro@gmail.com>
2024-07-19 23:33:08 +0300

    document functions
    use natspec
    
    more docs

--- contracts/BetMe.sol
+++ contracts/BetMe.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.8.24;
 
 import "hardhat/console.sol";
 
+/**
+ * @title BetMe
+ * @author MauriceDeSaxe
+ * @notice A simple 1v1 betting contract with a mediator.
+ */
 contract BetMe {
     address public mediator;
     address public bettor1;
@@ -16,18 +21,27 @@ contract BetMe {
     event Winner(uint256 amount, uint256 when, address who);
     event Withdraw(uint256 amount, uint256 when, address who);
 
+    /**
+     * @notice Mediator creates the contract assigning the bettors.
+     * @param _bettor1 Address of the first bettor.
+     * @param _bettor2 Address of the second bettor.
+     */
     constructor(address _bettor1, address _bettor2) payable {
         mediator = msg.sender;
         bettor1 = _bettor1;
         bettor2 = _bettor2;
     }
 
+    /**
+     * @notice Bettors can bet whatever they want, as soon as bets are equal the bets are locked.
+     * @dev You can't bet if you're not a bettor or if the bet is locked.
+     */
     function bet() public payable {
         bool isBettor1 = msg.sender == bettor1;
         bool isBettor2 = msg.sender == bettor2;
+        require(msg.value > 0, "Amount must be greater than 0");
         require(isBettor1 || isBettor2, "You are not a bettor");
         require(isLocked == false, "The bet is locked");
-        require(msg.value > 0, "Amount must be greater than 0");
 
         bets[msg.sender] += msg.value;
         if (bets[bettor1] == bets[bettor2]) {
@@ -36,16 +50,23 @@ contract BetMe {
         emit Bet(msg.value, block.timestamp, msg.sender);
     }
 
+    /**
+     * @notice The mediator can pick the winner if the bet is locked or the winner is already picked.
+     * @param _winner Address of the winner.
+     */
     function pickWinner(address _winner) public {
         require(msg.sender == mediator, "You are not the mediator");
-        require(winner == address(0), "The winner is already picked");
         require(isLocked == true, "The bet is not locked");
+        require(winner == address(0), "The winner is already picked");
         require(_winner == bettor1 || _winner == bettor2, "The winner is not a bettor");
 
         winner = _winner;
         emit Winner(bets[winner], block.timestamp, winner);
     }
 
+    /**
+     * @notice If the winner is picked, the winner can withdraw the total amount of deposits.
+     */
     function withdraw() public {
         require(winner != address(0), "The winner is not picked");
         require(msg.sender == winner, "You are not the winner");
